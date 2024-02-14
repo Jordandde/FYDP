94ab7c8e74a4e1e5566bd7b044d201c46558af27
Patrick Li <lipatrick.1023@gmail.com>
2024-02-13 22:57:20 -0500

    Skeleton for communication with FPGA. Removed Go stuff

--- backend/README.md
+++ backend/README.md
@@ -1,13 +1,13 @@
 # Getting started
-The backend of our project uses {insert cpp or go} to pass matrices from the frontend to the TCP connected FPGA. 
-This is done via json from the frontend to backend and an ethernet connection from the backend to the FPGA.
+The backend of our project uses C++ to pass matrices from the frontend to the TCP connected FPGA. 
+This is done via JSON from the frontend to backend and an ethernet connection from the backend to the FPGA.
 
 ## Objective of the backend
 The backend should do 4 things:
 1. Start up in the background when the desktop app icon is clicked
-2. listen for a json message, and then process the message when it is received
-3. package the received data into a useable package that can be attached to the TCP call and sent to the FPGA
-4. listen and wait for a response from the FPGA
+2. Listen for a JSON message, and then process the message when it is received
+3. Package the received data into a useable package that can be attached to the TCP call and sent to the FPGA
+4. Listen and wait for a response from the FPGA
 
 ## Precautions
 1. Make sure to add a timeout while the backend is listening to the FPGA to avoid hanging the whole application
@@ -15,19 +15,11 @@ The backend should do 4 things:
 
 
 ## Technology
-The backend should consist of a simple http server. I propose that we use Go for this, as it is the main use of Go and debugging it is extremely easy. Alternatives include Python, CPP, Rust, and C.
+The backend should consist of a simple http server. C++ will be used for this, largely for the sake of speed and being able to work effectively in parallel.
 
 ## Building
-build this in the backend directory by running ```go build```
-
-## Running
-Run this from the backend directory by running ```./backend```
-
-# C++
-Was struggling with Go. For the sake of speed and being able to work effectively in parallel, I translated the backend into C++...
-
-## Building (C++)
+Make sure you have xcode command line tools installed on Mac OS.
 `g++ -std=c++17 -o main main.cpp -lboost_system`
 
-## Running (C++)
+## Running
 `./main`
--- backend/main.cpp
+++ backend/main.cpp
@@ -13,12 +13,14 @@ using namespace boost::asio;
 // ||                             DEFINES                             ||
 // *********************************************************************
 #define POST_PORT 0xB00B
+#define FPGA_PAYLOAD_DIMS_SIZE 4 // num_rows1, num_cols1, num_rows2, num_cols2
 
 // *********************************************************************
 // ||                            FUNCTIONS                            ||
 // *********************************************************************
 // AI did this, I have no idea if this is legit -Pat
-void handle_options_request(ip::tcp::socket& socket) {
+void handle_options_request(ip::tcp::socket &socket)
+{
     // Send CORS headers for preflight request
     std::string response = "HTTP/1.1 200 OK\r\n";
     response += "Access-Control-Allow-Origin: *\r\n";
@@ -28,17 +30,20 @@ void handle_options_request(ip::tcp::socket& socket) {
     boost::asio::write(socket, boost::asio::buffer(response));
 }
 
-void handle_request(const std::string& request, ip::tcp::socket& socket) {
-    try {
-
+void handle_request(const std::string &request, ip::tcp::socket &socket)
+{
+    try
+    {
         std::cout << "Request received: " << request << std::endl;
         // Handle OPTIONS request
-        if (request.substr(0, 7) == "OPTIONS") {
+        if (request.substr(0, 7) == "OPTIONS")
+        {
             handle_options_request(socket);
             return;
         }
 
-        else if (request.substr(0, 4) != "POST") {
+        else if (request.substr(0, 4) != "POST")
+        {
             // Send a 405 Method Not Allowed response
             std::string response = "HTTP/1.1 405 Method Not Allowed\r\n";
             response += "Content-Type: text/plain\r\n";
@@ -48,7 +53,8 @@ void handle_request(const std::string& request, ip::tcp::socket& socket) {
             return;
         }
 
-        if (request.empty()) {
+        if (request.empty())
+        {
             // std::cout << "Empty request received." << std::endl;
             throw std::invalid_argument("Empty request received.");
             std::cout << "Empty request received." << std::endl;
@@ -57,49 +63,142 @@ void handle_request(const std::string& request, ip::tcp::socket& socket) {
 
         // Find the position of the first '{' character after the headers
         size_t json_start_pos = request.find("{");
-        if (json_start_pos == std::string::npos) {
+        if (json_start_pos == std::string::npos)
+        {
             throw std::invalid_argument("JSON data not found in request.");
         }
 
         // Extract the JSON payload from the request
-        std::string json_payload = request.substr(json_start_pos);
+        std::string in_json_payload = request.substr(json_start_pos);
 
         // Parse the JSON payload into the Matrices struct
-        auto matrices_payload = nlohmann::json::parse(json_payload)["matrices"];
+        auto in_matrices_payload = nlohmann::json::parse(in_json_payload)["matrices"];
 
         std::cout << "Matrices received successfully!" << std::endl;
 
-        Matrices matrices(matrices_payload[0], matrices_payload[1]);
+        if (in_matrices_payload.size() != 2)
+        {
+            std::cout << "Expected 2 matrices, received " << in_matrices_payload.size() << " matrices." << std::endl;
+        }
+
+        Matrices matrices(in_matrices_payload[0], in_matrices_payload[1]);
 
         // Print input matrices as sanity check
         std::cout << "Input Matrix 1:" << std::endl;
-        for (const auto& row : matrices.input_matrix_1) {
-            for (const auto& cell : row) {
+        for (const auto &row : matrices.input_matrix_1)
+        {
+            for (const auto &cell : row)
+            {
                 std::cout << cell << " ";
             }
             std::cout << std::endl;
         }
-        
+
         std::cout << "Input Matrix 2:" << std::endl;
-        for (const auto& row : matrices.input_matrix_2) {
-            for (const auto& cell : row) {
+        for (const auto &row : matrices.input_matrix_2)
+        {
+            for (const auto &cell : row)
+            {
                 std::cout << cell << " ";
             }
             std::cout << std::endl;
         }
 
         // CONVERT FLOATS TO FPGA/HARDWARE FRIENDLY FORMAT, PROBABLY INTS??
+        // Let's just start with ints for now. Floats can be handled with fixed precision by mulitplying by factor(s) of 10 and scaling the result accordingly
+        size_t out_fpga_payload_size = (FPGA_PAYLOAD_DIMS_SIZE + matrices.input_matrix_1.get_num_rows() * matrices.input_matrix_1.get_num_cols() + 
+                                    matrices.input_matrix_2.get_num_rows() * matrices.input_matrix_2.get_num_cols());
+
+        int* out_fpga_payload = (int*)malloc(out_fpga_payload_size * sizeof(int)); // Using int sizes for now. Can use smaller sizes if we think it's worth it & after we come up with size constraints
+
+        int pos = 0;
+
+        // Copy in dimensions of input matrix 1
+        out_fpga_payload[pos++] = matrices.input_matrix_1.get_num_rows();
+        out_fpga_payload[pos++] = matrices.input_matrix_1.get_num_cols();
+        
+        // Copy in input matrix 1
+        for (int i = 0; i < matrices.input_matrix_1.get_num_rows(); i++)
+        {
+            for (int j = 0; j < matrices.input_matrix_1.get_num_cols(); j++)
+            {
+                out_fpga_payload[pos++] = static_cast<int>(matrices.input_matrix_1[i][j]);
+            }
+        }
+
+        // Copy in dimensions of input matrix 2
+        out_fpga_payload[pos++] = matrices.input_matrix_2.get_num_rows();
+        out_fpga_payload[pos++] = matrices.input_matrix_2.get_num_cols();
+
+        // Copy in input matrix 2
+        for (int i = 0; i < matrices.input_matrix_2.get_num_rows(); i++)
+        {
+            for (int j = 0; j < matrices.input_matrix_2.get_num_cols(); j++)
+            {
+                out_fpga_payload[pos++] = static_cast<int>(matrices.input_matrix_2[i][j]);
+            }
+        }
+
+        if (pos != out_fpga_payload_size)
+        {
+            std::cout << "Error: pos = " << pos << ", out_fpga_payload_size = " << out_fpga_payload_size << std::endl;
+        }
+
+        // Print FPGA payload for debugging
+        std::cout << "Out FPGA Payload:" << std::endl;
+        for (int i = 0; i < out_fpga_payload_size; i++)
+        {
+            std::cout << out_fpga_payload[i];
+        }
+        std::cout << std::endl;
+
         // TALK WITH FPGA AND GET RESULTS
-        // SCALE RESULTS "12-bit dac with 4.096 reference voltage -> analog switch with 15ohms of resistance 
+        // Boilerplate from ChatGPT (does this socket connection stuff have to happen every time?? Or can it be put in main() and run once?)
+        // boost::asio::io_context io_context;
+
+        // // Resolve the server address and port
+        // tcp::resolver resolver(io_context);
+        // tcp::resolver::results_type endpoints = resolver.resolve(server_address, server_port);
+
+        // // Establish a connection to the server
+        // tcp::socket socket(io_context);
+        // boost::asio::connect(socket, endpoints);
+
+        // // Send data to the server
+        
+        // Send the payload to the FPGA
+        // boost::asio::write(socket, boost::asio::buffer((void*)out_fpga_payload, out_fpga_payload_size * sizeof(int));
+
+        free(out_fpga_payload);
+        
+
+        // Receive the result from the FPGA
+        // size_t in_fpga_payload_size = matrices.result_matrix.get_num_rows() * matrices.result_matrix.get_num_cols();
+
+        // int* in_fpga_payload = (int*)malloc(in_fpga_payload_size * sizeof(int));
+
+        // size_t bytes_transferred = socket.read_some(boost::asio::buffer((void*)in_fpga_payload, in_fpga_payload_size * sizeof(int));
+        // Print result from FPGA
+        // std::cout << "In FPGA Payload:" << std::endl;
+        // for (int i = 0; i < in_fpga_payload_size; i++)
+        // {
+        //     std::cout << in_fpga_payload[i];
+        // }
+        // std::cout << std::endl;
+
+        // SCALE RESULTS "12-bit dac with 4.096 reference voltage -> analog switch with 15ohms of resistance
         // -> analog multipler that divides the result by a factor of 10 -> 16-bit adc with 4.096 reference voltage"
 
         // UPDATE RESULT MATRIX
         // TODO: REMOVE THIS DUMMY OPERATION
         // Multiply matrices 1 and 2 and store the result in the result matrix
         // This is a leetcode question I think LOL
-        for (int i = 0; i < matrices.input_matrix_1.get_num_cols(); i++) {
-            for (int j = 0; j < matrices.input_matrix_2.get_num_rows(); j++) {
-                for (int k = 0; k < matrices.input_matrix_2.get_num_cols(); k++) {
+        for (int i = 0; i < matrices.input_matrix_1.get_num_cols(); i++)
+        {
+            for (int j = 0; j < matrices.input_matrix_2.get_num_rows(); j++)
+            {
+                for (int k = 0; k < matrices.input_matrix_2.get_num_cols(); k++)
+                {
                     matrices.result_matrix[i][j] += matrices.input_matrix_1[i][k] * matrices.input_matrix_2[k][j];
                 }
             }
@@ -107,8 +206,10 @@ void handle_request(const std::string& request, ip::tcp::socket& socket) {
 
         // Print result matrix for debugging
         std::cout << "Result Matrix:" << std::endl;
-        for (const auto& row : matrices.result_matrix) {
-            for (const auto& cell : row) {
+        for (const auto &row : matrices.result_matrix)
+        {
+            for (const auto &cell : row)
+            {
                 printf("%.2f ", cell);
             }
             std::cout << std::endl;
@@ -126,7 +227,9 @@ void handle_request(const std::string& request, ip::tcp::socket& socket) {
         response_stream << "Content-Length: " << response.size() << "\r\n\r\n";
         response_stream << response;
         boost::asio::write(socket, response_buf);
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception &e)
+    {
         std::cerr << "Error handling request: " << e.what() << std::endl;
         // Send an error response back to the frontend
         std::string response = "Error handling request: ";
@@ -140,12 +243,13 @@ void handle_request(const std::string& request, ip::tcp::socket& socket) {
         response_stream << response;
         boost::asio::write(socket, response_buf);
     }
-
 }
 
 // Completely AI-cooked, I have no idea what's going on here but it looks alright to me and it works
-void start_server() {
-    try {
+void start_server()
+{
+    try
+    {
         // Create io_context
         io_context io;
 
@@ -155,7 +259,8 @@ void start_server() {
         std::cout << "Server started. Waiting for incoming connections..." << std::endl;
 
         // Start accepting connections
-        while (true) {
+        while (true)
+        {
             // Create a socket for the incoming connection
             ip::tcp::socket socket(io);
 
@@ -166,21 +271,22 @@ void start_server() {
             std::string request;
             boost::system::error_code error;
             size_t bytes_transferred;
-            do {
-                char buffer[1024]; // Adjust buffer size as needed
-                bytes_transferred = socket.read_some(boost::asio::buffer(buffer), error);
-                request.append(buffer, buffer + bytes_transferred);
-            } while (0);
-            // Idk why but the original thing chatgpt suggested was this with no loop and the requests would just be zero length?? 
-            // Then it suggested do-while(bytes_transferred > 0) but that would just hang forever so I just put 0 and it works fine
+            char buffer[1024]; // Adjust buffer size as needed
+            bytes_transferred = socket.read_some(boost::asio::buffer(buffer), error);
+            request.append(buffer, buffer + bytes_transferred);
 
-            if (error == boost::asio::error::eof) {
+            if (error == boost::asio::error::eof)
+            {
                 // Connection closed cleanly by peer
                 std::cout << "Connection closed by peer." << std::endl;
-            } else if (error) {
+            }
+            else if (error)
+            {
                 // Some other error
                 throw boost::system::system_error(error); // Rethrow the error
-            } else {
+            }
+            else
+            {
                 // Trim the request string to actual size
                 request.resize(bytes_transferred);
 
@@ -188,12 +294,15 @@ void start_server() {
                 handle_request(request, socket);
             }
         }
-    } catch (std::exception& e) {
+    }
+    catch (std::exception &e)
+    {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
 }
 
-int main() {
+int main()
+{
     start_server();
     return 0;
 }
